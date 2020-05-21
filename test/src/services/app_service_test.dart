fa153a867fcdef6c0dc53524df68742cb06a6bc1
bwnyasse <risbonyasse@gmail.com>
2020-05-21 14:45:08 -0400

    test(app_service): UnimplementedError

---
+++ test/src/services/app_service_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  test('loadMovies', () async {
+    // TODO 1- Must implement app_service#loadMovies
+    throw UnimplementedError();
+  });
+}
