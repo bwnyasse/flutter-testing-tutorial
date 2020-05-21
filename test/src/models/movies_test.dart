ab5a7a5817ad022a0063f1dc98d610ea9272d1e0
bwnyasse <risbonyasse@gmail.com>
2020-05-21 14:53:46 -0400

    test(models/movies): posterPath is null

---
+++ test/src/models/movies_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_movie_deep_dive_test/src/models/models.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('movie.posterPathResolved', () {
+    test('posterPath is null', () {
+      //TODO 1- Test the case movie.posterPathResolved is null
+    });
+  });
+}
