a6ef20470359b7def4876e2cba2f828f64622a62
bwnyasse <risbonyasse@gmail.com>
2020-05-21 14:47:41 -0400

    test(app_service): loadmovies

--- test/src/services/app_service_test.dart
+++ test/src/services/app_service_test.dart
@@ -1,8 +1,21 @@
+import 'dart:convert';
+
+import 'package:flutter_movie_deep_dive_test/src/models/models.dart';
+import 'package:flutter_movie_deep_dive_test/src/services/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+import '../common.dart';
 
 main() {
   test('loadMovies', () async {
-    // TODO 1- Must implement app_service#loadMovies
-    throw UnimplementedError();
+    final mockClient = MockClient((request) async {
+      return Response(json.encode(exampleJsonResponse), 200);
+    });
+    final service = AppService(mockClient);
+    final expectedResponse = MoviesResponse.fromJson(exampleJsonResponse);
+    final actualResponse = await service.loadMovies();
+    expect(actualResponse, equals(expectedResponse));
   });
 }
