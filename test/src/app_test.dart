0a3d57415d9348485670eaf9d8bccd5054e11d44
bwnyasse <risbonyasse@gmail.com>
2020-05-21 16:01:52 -0400

    test(app): init widgets testing

---
+++ test/src/app_test.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:image_test_utils/image_test_utils.dart';
+import 'package:flutter_movie_deep_dive_test/src/providers/providers.dart';
+import 'package:flutter_movie_deep_dive_test/src/widgets/widgets.dart';
+import 'package:flutter_movie_deep_dive_test/src/app.dart';
+
+import 'common.dart';
+
+void main() {
+  testWidgets('Display App', (WidgetTester tester) async {
+      await tester.pumpWidget(AppProvider(
+        httpClient: MockClient((request) async {
+          return Response(json.encode(exampleJsonResponse), 200);
+        }),
+        child: MyApp(),
+      ));
+
+      //TODO : 1- Use Finder to match the widget named MyHomePage
+  });
+}
