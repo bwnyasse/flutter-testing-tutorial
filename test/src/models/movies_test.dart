b0d943b64de87a635eaf619a11aaa42ccef52546
bwnyasse <risbonyasse@gmail.com>
2020-05-21 14:54:43 -0400

    test(models/movies): posterPath is valid

--- test/src/models/movies_test.dart
+++ test/src/models/movies_test.dart
@@ -4,7 +4,14 @@ import 'package:flutter_test/flutter_test.dart';
 main() {
   group('movie.posterPathResolved', () {
     test('posterPath is null', () {
-      //TODO 1- Test the case movie.posterPathResolved is null
+      Movie m = Movie();
+      expect(m.posterPathResolved, equals('https://via.placeholder.com/300'));
+    });
+
+    test('posterPath is valid', () {
+      Movie m = Movie(posterPath: 'some-value');
+      expect(m.posterPathResolved,
+          equals('http://image.tmdb.org/t/p/w185/some-value'));
     });
   });
 }
