783feef5cd139955dc0dec3cebf961eae05eb016
bwnyasse <risbonyasse@gmail.com>
2020-05-21 14:49:18 -0400

    test(app_service): group

--- test/src/services/app_service_test.dart
+++ test/src/services/app_service_test.dart
@@ -9,13 +9,17 @@ import 'package:http/testing.dart';
 import '../common.dart';
 
 main() {
-  test('loadMovies', () async {
-    final mockClient = MockClient((request) async {
-      return Response(json.encode(exampleJsonResponse), 200);
+  group('loadMovies', () {
+    test('status == 200', () async {
+      final mockClient = MockClient((request) async {
+        return Response(json.encode(exampleJsonResponse), 200);
+      });
+      final service = AppService(mockClient);
+      final expectedResponse = MoviesResponse.fromJson(exampleJsonResponse);
+      final actualResponse = await service.loadMovies();
+      expect(actualResponse, equals(expectedResponse));
     });
-    final service = AppService(mockClient);
-    final expectedResponse = MoviesResponse.fromJson(exampleJsonResponse);
-    final actualResponse = await service.loadMovies();
-    expect(actualResponse, equals(expectedResponse));
+
+    //TODO 1- Must test the case status != 200
   });
 }
