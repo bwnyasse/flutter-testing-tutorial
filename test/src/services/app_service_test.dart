0dba926423d67e8cc1ff30270fb9af1e9545af7e
bwnyasse <risbonyasse@gmail.com>
2021-11-09 15:30:42 -0500

    step-2-TODO

--- test/src/services/app_service_test.dart
+++ test/src/services/app_service_test.dart
@@ -9,14 +9,17 @@ import 'package:http/testing.dart';
 import '../common.dart';
 
 main() {
-  test('loadMovies', () async {
-    // RESOLVE 1- Must implement app_service#loadMovies
-    final mockClient = MockClient((request) async {
-      return Response(json.encode(exampleJsonResponse), 200);
+  group('loadMovies', () {
+    test('status == 200', () async {
+      final mockClient = MockClient((request) async {
+        return Response(json.encode(exampleJsonResponse), 200);
+      });
+      final service = AppService(mockClient);
+      final expectedResponse = MoviesResponse.fromJson(exampleJsonResponse);
+      final actualResponse = await service.loadMovies();
+      expect(actualResponse, equals(expectedResponse));
     });
-    final service = AppService(mockClient);
-    final expectedResponse = MoviesResponse.fromJson(exampleJsonResponse);
-    final actualResponse = await service.loadMovies();
-    expect(actualResponse, equals(expectedResponse));
+
+    //TODO 1- Must test the case status != 200
   });
 }
