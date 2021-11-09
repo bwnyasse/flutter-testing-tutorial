5ab98d24a012e38a3faca6f6e300ada7c1b2397d
bwnyasse <risbonyasse@gmail.com>
2021-11-09 15:24:09 -0500

    step-1-TODO

--- test/src/services/app_service_test.dart
+++ test/src/services/app_service_test.dart
@@ -1,36 +1,8 @@
-import 'dart:convert';
-
-import 'package:flutter_movie_deep_dive_test/src/models/models.dart';
-import 'package:flutter_movie_deep_dive_test/src/services/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
-import 'package:http/testing.dart';
-
-import '../common.dart';
 
 main() {
-  group('loadMovies', () {
-    test('status == 200', () async {
-      final mockClient = MockClient((request) async {
-        return Response(json.encode(exampleJsonResponse), 200);
-      });
-      final service = AppService(mockClient);
-      final expectedResponse = MoviesResponse.fromJson(exampleJsonResponse);
-      final actualResponse = await service.loadMovies();
-      expect(actualResponse, equals(expectedResponse));
-    });
-
-    test('status != 200', () async {
-      final mockClient = MockClient((request) async {
-        return Response(json.encode(exampleJsonResponse), 500);
-      });
-      final service = AppService(mockClient);
-      expect(
-        () async => await service.loadMovies(),
-        throwsA(predicate((e) =>
-            e is LoadMoviesException &&
-            e.message == 'LoadMovies - Request Error: 500')),
-      );
-    });
+  test('loadMovies', () async {
+    // TODO 1- Must implement app_service#loadMovies
+    throw UnimplementedError();
   });
 }
